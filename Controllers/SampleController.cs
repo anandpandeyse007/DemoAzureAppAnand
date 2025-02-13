df737085c768a36938ebf01e5d8282f4460a0311
anandpandeyse007 <Anand.Pandey2@ibm.com>
2025-02-13 12:22:56 +0530

    test code

---
+++ Controllers/SampleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mvc_web_azure_app_anand.Controllers
+{
+    public class SampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
